521cf71af5e71619eafbf08b97fa133a93be9e7e
Ashish Kumar <ashishkhkhr04@gmail.com>
2021-12-21 22:01:50 +0400

    Created the changes to invoke the lambda

--- src/main/java/com/data/tree/api/LambdaHandler.java
+++ src/main/java/com/data/tree/api/LambdaHandler.java
@@ -1,6 +1,5 @@
 package com.data.tree.api;
 
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.data.tree.api.Model.RequestVO;
@@ -10,7 +9,10 @@ public class LambdaHandler implements RequestHandler<RequestVO, ResponseVO> {
 
     @Override
     public ResponseVO handleRequest(RequestVO input, Context context) {
-        return null;
+        ResponseVO responseVO = new ResponseVO();
+        responseVO.setLatitude(1027431.148);
+        responseVO.setLongitude(202756.7687);
+        return responseVO;
     }
 
 }
